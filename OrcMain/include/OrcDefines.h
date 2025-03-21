14916dae832a4c23baabd8c9621d0bd7e88eeb8e
Uzi Wang <429373960@qq.com>
2025-03-21 10:55:47 +0800

    Fix warning
    warning: backslash-newline at end of file

--- OrcMain/include/OrcDefines.h
+++ OrcMain/include/OrcDefines.h
@@ -4,4 +4,4 @@
     ClassName(const ClassName&) = delete;            \
     ClassName& operator=(const ClassName&) = delete; \
     ClassName(ClassName&&) = delete;                 \
-    ClassName& operator=(ClassName&&) = delete;
+    ClassName& operator=(ClassName&&) = delete;      \
